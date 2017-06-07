a6f01991e24e87b7dd5c2dc6f8cfb9756fca7be4
adamdeeking <arivera@kippnyccp.org>
2017-06-07 11:19:36 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("img").click(function(){
     alert("you clicked on an image!");
-    $(document).ready(function(){
+    $(p).ready(function(){
   $("img").click(function(){
   });
   
